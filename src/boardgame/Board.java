f7d417193cdbae74b36f9ec81c3bb36a3fb8034d
Nailton Sousa <nailton.osn@gmail.com>
2023-01-28 15:46:35 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -26,4 +26,12 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+     public Piece price(int row, int column){
+         return pieces[row][column];
+     }
+    public Piece price(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 }
---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,22 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+    private Board board;
+
+    public ChessMatch() {
+        board = new Board(8, 8);
+    }
+
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getRows()];
+
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.price(i, j);
+            }
+        }
+        return mat;
+    }
+}
