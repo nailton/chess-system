ff7dfd77b5c5ffe5939517a209adab8ef3ff8497
Nailton Sousa <nailton.osn@gmail.com>
2023-01-28 15:18:48 -0300

    First class: Postion

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
