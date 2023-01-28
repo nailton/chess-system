5b9df51942ccb293cab041813bd0e086e2a13eb2
Nailton Sousa <nailton.osn@gmail.com>
2023-01-28 20:35:09 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -18,7 +18,7 @@ public class ChessMatch {
 
         for (int i = 0; i < board.getRows(); i++) {
             for (int j = 0; j < board.getColumns(); j++) {
-                mat[i][j] = (ChessPiece) board.price(i, j);
+                mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
